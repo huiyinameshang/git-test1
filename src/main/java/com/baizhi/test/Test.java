ac226275aa9e8b50ca00509f475ad318570a7cc8
lsh <353058493@qq.com>
2018-10-19 17:35:40 +0800

    这是第五次提交

--- src/main/java/com/baizhi/test/Test.java
+++ src/main/java/com/baizhi/test/Test.java
@@ -5,9 +5,7 @@ package com.baizhi.test;
  */
 public class Test {
     public static void main(String[] args) {
-        System.out.println("你好");
-        System.out.println("hello");
-        System.out.println("怎么");
+
 
     }
 }
