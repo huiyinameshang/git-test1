f5926cb8c634974370ba255294aeba63ca3699c0
lsh <353058493@qq.com>
2018-10-19 16:53:57 +0800

    这是第四次提交

--- src/main/java/com/baizhi/test/Test.java
+++ src/main/java/com/baizhi/test/Test.java
@@ -7,6 +7,7 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("你好");
         System.out.println("hello");
+        System.out.println("怎么");
 
     }
 }
